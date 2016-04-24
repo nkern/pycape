e47c958c1d616d2b2152105eefa2fe0d5b26c668
Nicholas S. Kern <nkern@umich.edu>
2016-04-24 10:00:15 -0700

    modified:   emulate.py

---
+++ emulate.py
@@ -0,0 +1,17 @@
+"""
+emulate.py : functions for emulating the 21cm PS
+
+"""
+
+
+class emu(dic):
+
+	def __init__(self,dic):
+		self.__dict__.update(dic)
+
+	def update(self,dic):
+		self.__dict__.update(dic)
+
+
+
+
