3df98c0006815ec4dd8d4664cfc4c0fae4b6fd21
Nicholas S. Kern <nkern@umich.edu>
2016-04-27 22:21:01 -0700

    new file:   drive_sampler_mpi.py 	new file:   mpi_chains/README.txt

---
+++ drive_sampler_mpi.py
@@ -0,0 +1,30 @@
+"""
+used in conjunction with toolbox.drive_sampler_mpi() to run sampler in parallel
+"""
+from mpi4py import MPI
+import sys
+import emcee
+import cPickle as pkl
+
+size = MPI.COMM_WORLD.Get_size()
+rank = MPI.COMM_WORLD.Get_rank()
+name = MPI.Get_processor_name()
+sys.stdout.write("Hello, World! I am process %d of %d on %s.\n"% (rank, size, name))
+
+# Load workspace and initial positions of walkers
+file = open('Workspace.pkl','rb')
+input = pkl.Unpickler(file)
+W = input.load()
+file.close()
+
+# Initialize sampler
+W.sampler_init(W.sampler_init_kwargs)
+
+# Run chains
+W.drive_sampler(pos[rank],step_num=W.step_num,burn_num=W.burn_num)
+
+# Output chains
+file = open(self.dir_pycape+'/mpi_chains/mpi_chain_rank%s.pkl'%rank,'wb')
+output = pkl.Pickler(file)
+output.dump({'rank%s_chain'%rank:W.S.sampler.chain})
+file.close()
