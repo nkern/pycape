43e349fad2fac37afa730c1a382dd88989ff930e
Nicholas S. Kern <nkern@umich.edu>
2016-04-29 14:26:19 -0700

    new file:   common_priors.py

---
+++ common_priors.py
@@ -0,0 +1,27 @@
+"""
+common_priors.py : a list of common priors on physical parameters relevant to Cosmic Dawn and EoR studies
+
+"""
+
+class common_priors():
+
+	def __init__(self,dic):
+		self.__dict__.update(dic)
+
+	def neutral_frac_prior(self):
+		pass
+
+	def brightness_temp_prior(self):
+		pass
+
+	def cosmological_priors(self):
+		pass
+
+	def astrophysical_priors(self):
+		pass
+
+	def CMB_priors(self):
+		pass
+
+
+
