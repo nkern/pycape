5f8c18d661836581aaeb43fe8972c4943ba33ff0
Nicholas S. Kern <nkern@umich.edu>
2016-06-12 12:32:59 -0700

    modified:   common_priors.py

--- common_priors.py
+++ common_priors.py
@@ -8,34 +8,34 @@ Planck Collaboration (1605.03507)
 """
 
 cmb_priors1 = {
-'name'			= 'Planck TT,TE,EE+lowP',
-'ombh2'			= 0.02222,
-'ombh2_err'		= 0.00023,
-'omch2'			= 0.1197,
-'omch2_err'		= 0.0022,
-'theta_mc100'		= 1.04085,
-'theta_mc100_err'	= 0.00047,
-'ns'			= 0.9655,
-'ns_err'		= 0.0062,
-'lntentenAs'		= 3.089,
-'lntentenAs_err'	= 0.036,
-'tau'			= 0.078,
-'tau_err'		= 0.019}
+'name'			: 'Planck TT,TE,EE+lowP',
+'ombh2'			: 0.02222,
+'ombh2_err'		: 0.00023,
+'omch2'			: 0.1197,
+'omch2_err'		: 0.0022,
+'theta_mc100'		: 1.04085,
+'theta_mc100_err'	: 0.00047,
+'ns'			: 0.9655,
+'ns_err'		: 0.0062,
+'lntentenAs'		: 3.089,
+'lntentenAs_err'	: 0.036,
+'tau'			: 0.078,
+'tau_err'		: 0.019}
 
 cmb_priors2 = {
-'name'			= 'Planck TT+lollipop',
-'ombh2'                 = 0.02222,
-'ombh2_err'             = 0.00023,
-'omch2'                 = 0.1205,
-'omch2_err'             = 0.0022,
-'theta_mc100'           = 1.04085,
-'theta_mc100_err'       = 0.00047,
-'ns'                    = 0.9620,
-'ns_err'                = 0.0050,
-'lntentenAs'            = 3.059,
-'lntentenAs_err'        = 0.020,
-'tau'                   = 0.058,
-'tau_err'               = 0.012}
+'name'			: 'Planck TT+lollipop',
+'ombh2'                 : 0.02222,
+'ombh2_err'             : 0.00023,
+'omch2'                 : 0.1205,
+'omch2_err'             : 0.0022,
+'theta_mc100'           : 1.04085,
+'theta_mc100_err'       : 0.00047,
+'ns'                    : 0.9620,
+'ns_err'                : 0.0050,
+'lntentenAs'            : 3.059,
+'lntentenAs_err'        : 0.020,
+'tau'                   : 0.058,
+'tau_err'               : 0.012}
 
 
 
