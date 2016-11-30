bbd2630abc50f04ae6529185ebe41579da772252
Nicholas S. Kern <nkern@berkeley.edu>
2016-11-29 21:01:51 -0800

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 import sys
 import os
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 setup(
     name        = 'pycape',
-    version     = 0.1,
+    version     = '0.1',
+    description = 'emulator for 21cm studies',
     author      = 'Nick Kern',
     url         = "http://github.com/nkern/pycape",
     packages    = ['pycape','pycape.scripts','pycape.tests',\
