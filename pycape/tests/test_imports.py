883fbe145cf537dc1c2f01fd69285d0b282177e5
Nicholas S. Kern <nkern@berkeley.edu>
2016-11-29 19:06:38 -0800

    modified:   .travis.yml 	deleted:    pycape/tests/__init__.py 	modified:   pycape/tests/test_imports.py

--- pycape/tests/test_imports.py
+++ pycape/tests/test_imports.py
@@ -1,2 +1,31 @@
-def test_import1():
-    import numpy as np
+from unittest import TestCase
+
+class TestOne(TestCase):
+
+
+    def test_import(self):
+        try:
+            import numpy
+        except:
+            self.fail('failed importing numpy')
+
+
+    def test_import2(self):
+        try:
+            import scipy
+        except:
+            self.fail('failed scipy')
+
+
+    def test_import3(self):
+        try:
+            import whatthe
+        except:
+            self.fail('failed import')
+
+
+    def test_import4(self):
+        try:
+            import sklearn
+        except:
+            self.fail('failed import')
