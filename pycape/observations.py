ba4c54ac8d764ec1b1d6a16f3ede5dd48a2a7798
Nicholas S. Kern <nkern@berkeley.edu>
2016-12-11 18:44:49 -0800

    modified:   observations.py

--- pycape/observations.py
+++ pycape/observations.py
@@ -62,7 +62,10 @@ class Obs(object):
         self.ydata_cat      = ydata_cat
         self.cat_types      = cat_types
 
-        self.x_ext          = np.concatenate(list(self.xdata))
+        try:
+            self.x_ext          = np.concatenate(list(self.xdata))
+        except:
+            self.x_ext          = self.xdata
         self.N_data         = len(self.x_ext)
         self.cov            = np.eye(self.N_data)*self.yerrs**2
         self.invcov         = la.inv(self.cov)
