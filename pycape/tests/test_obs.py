e21b4a9d934ae8507b7dea99613b00346c933bf2
Nicholas S. Kern <nkern@berkeley.edu>
2016-12-11 16:14:22 -0800

    new file:   pycape/tests/test_obs.py

---
+++ pycape/tests/test_obs.py
@@ -0,0 +1,25 @@
+import numpy as np
+import unittest
+from pycape import Obs
+import warnings
+from py21cmsense import calc_sense
+
+class TestObs(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_21cmsense(self):
+        
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
