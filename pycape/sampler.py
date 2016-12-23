5843c414220f31c1faf2eae4b278b96eded4e670
Nicholas S. Kern <nkern@berkeley.edu>
2016-12-23 11:35:31 -0500

    modified:   sampler.py

--- pycape/sampler.py
+++ pycape/sampler.py
@@ -318,7 +318,7 @@ class Samp(object):
         lnlike = self.lnlike(self.O.ydata,self.model_ydata,self.data_invcov) 
 
         # Evaluate lnprior
-        lnprior = self.lnprior(theta, sphere=lnlike_kwargs['predict_kwargs']['sphere'])
+        lnprior = self.lnprior(theta, **lnlike_kwargs['predict_kwargs'])
 
         # Output lnprob or lnlike
         if out_lnlike == True:
