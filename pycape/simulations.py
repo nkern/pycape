a33d566792ca974d555367c81ba809f94dd0500d
Nicholas Kern <nkern@berkeley.edu>
2017-08-12 12:14:51 -0700

    modified:   pycape/simulations.py

--- pycape/simulations.py
+++ pycape/simulations.py
@@ -23,10 +23,20 @@ class Drive_Camb(object):
         self.pars = camb.CAMBparams()
         self.reion_pars = camb.ReionizationParams()
 
-    def set_params(self,H0=67.5,ombh2=0.022,omch2=0.122,mnu=0.06,omk=0,tau=None,ns=0.965,As=2e-9,theta_mc=None):
+    def set_params(self,H0=67.27,ombh2=0.02225,omch2=0.1184,mnu=0.06,omk=0,tau=0.079,ns=0.945,As=2.2065e-9,theta_mc=None,
+                        omb=None, omc=None):
         """
-        set_params(H0=67.5,ombh2=0.022,omch2=0.122,mnu=0.06,omk=0,tau=None,ns=0.965,As=2e-9,theta_mc=None)
+        Set cosmological parameters
+        Default values consistent with Planck 2016 Parameter Constraints with TT, TE, EE + lowP
+        Note: ombh2 and omb, omch2 and omc cannot simultaneously be set. If both are assigned, will default
+        to omb and omc values.
         """
+        if ombh2 is not None and omb is not None:
+            ombh2 = omb * (H0/100.0)**2
+        if omch2 is not None and omc is not None:
+            omch2 = omc * (H0/100.0)**2
+
+        # Set camb parameters
         self.ns = ns
         self.pars.set_cosmology(H0=H0, ombh2=ombh2, omch2=omch2, cosmomc_theta=theta_mc, omk=omk, tau=tau)
         self.pars.set_dark_energy()
@@ -67,5 +77,14 @@ class Drive_Camb(object):
     def get_pars(self):
         return OrderedDict(zip(['sigma8','theta_mc','hlittle','As','ombh2','omch2','ns','tau'],\
                     [self.sigma8,self.theta_mc,self.hlittle,self.As,self.ombh2,self.omch2,self.ns,self.tau]))
+
+    def get_cl(self):
+        self.total = self.results.get_cmb_power_spectra(params=self.pars, spectra=['total'],CMB_unit='muK')['total']
+
+
+
+
+
+
 
 
